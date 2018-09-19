5a92a700d62839d60331b954e00fdc4a7934180c
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-09-19 09:14:15 -0300

    TP1

--- README.md
+++
@@ -1,2 +0,0 @@
-# tp_laboratorio_2
-Trabajos prácticos de Laboratorio ||
---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        
+        public static double Operar(Numero numero1, Numero numero2, string operador) 
+        {
+            double retorno;
+            string valido;
+            valido = ValidarOperador(operador);
+
+            switch (valido)
+            {
+                case "/":
+                    retorno = numero1 / numero2;
+                    break;
+                case "-":
+                    retorno = numero1 - numero2;
+                    break;      
+                case "+":
+                    retorno = numero1 + numero2;
+                    break;
+                default:
+                    retorno = numero1 * numero2;
+                    break;
+            }
+            
+            return retorno;
+        }
+        
+        private static string ValidarOperador(string operador)
+        {
+            if (operador != "+" && operador != "-" && operador != "/" && operador != "*")
+            {
+                operador = "+";
+            }
+
+            return operador;
+        }
+    }
+    
+}
