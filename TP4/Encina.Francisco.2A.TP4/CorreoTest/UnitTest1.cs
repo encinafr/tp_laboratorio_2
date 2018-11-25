7fb17a9352f3f12eecdf43c97952b4fee2ac30f6
encinafr <encinajavier7@gmail.com>
2018-11-25 09:54:33 -0300

    Tests Aregados

---
+++ TP4/Encina.Francisco.2A.TP4/CorreoTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using MainCorreo;
+
+namespace CorreoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInstancia()
+        {
+            Correo correo = new Correo();
+            Assert.IsNotNull(correo.Paquetes);
+        }
+
+        [TestMethod]
+        public void TestTrackingId()
+        {
+            Paquete aux1 = new Paquete("TestRepetido1", "126");
+            Paquete aux2 = new Paquete("TestRepetido2", "126");
+            Correo correo = new Correo();
+            correo += aux1;
+
+            try
+            {
+                correo += aux2;
+                Assert.Fail();
+            }
+            catch(TrackingIdRepetidoException)
+            {
+               
+
+            }
+        }
+    }
+}
